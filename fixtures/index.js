2deb78116d6618eb6ad9fbe7ff63ab318c34e56c
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-08 02:12:57 +0200

    edit fixtures

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,14 +1,8 @@
 import fileResolved from './local';
 import fileUnresolved from './local-extra';
 
-console.log(localResolved, localUnresolved);
-
 import folderResolved from './folder';
 import folderUnresolved from './folder-extra';
 
-console.log(folderResolved, folderUnresolved);
-
 import pkgResolved from 'pkg';
 import pkgUnresolved from 'pkg-extra';
-
-console.log(pkgResolved, pkgUnresolved);
